cc2b656a53069c45a8eaef0a44b67a26c81b42c1
Jason Tokoph <jason@tokoph.net>
2014-10-28 16:35:28 -0700

    Cleaned up a few things and added fancy 'run' logic

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ var requests = require("request");
 var tmp = require("tmp");
 
 var toType = function(obj) {
-  return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+  return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
 };
 
 module.exports = {
-  run: function(block, data, callback, api_key) {
-    data = typeof data !== 'undefined' ? data : {};
+  run: function(block, data, api_key, callback) {
+    // allow data and api_key to be optional
+    if (typeof data === 'function') {
+      callback = data;
+      data = {};
+      api_key = null;
+    }
+    // allow api_key to be optional, but keep callback as last argument
+    else if (typeof api_key === 'function') {
+      callback = api_key;
+      api_key = null;
+    }
+
+    data = typeof data !== 'undefined' || data === null ? data : {};
     api_key = typeof api_key !== 'undefined' ? api_key : null;
 
     if (!(toType( data ) == "object")){
@@ -30,6 +42,7 @@ module.exports = {
       try {
         body = JSON.parse(body);
       } catch(e) {};
+
       if (callback){
         callback(body);
       }
@@ -43,14 +56,7 @@ module.exports = {
 
     var request = {
       params: {},
-      errors: [],
-      stdin: '',
-      getErrors: function(){
-          return this.errors;
-      },
-      addError: function(error){
-        this.errors.push(error);
-      }
+      errors: []
     };
 
     var response = {
@@ -70,21 +76,21 @@ module.exports = {
         libUsed = true;
         var filename = path.basename(filepath);
 
-        fs.readFile(filepath, function (err, data) {
+        fs.readFile(filepath, { encoding: 'base64' }, function (err, data) {
           if (err) throw err;
 
           this.result[name] = {
             filename: filename,
             mimeType: mime.lookup(filepath),
-            data: data.toString('base64')
+            data: data
           };
 
           if (callback) {
             callback();
           }
-
-          return this;
         }.bind(this));
+
+        return this;
       },
       addErrorOutput: function(title, message, callback) {
         libUsed = true;
@@ -94,7 +100,7 @@ module.exports = {
           title: title,
           message: message
         })
-        
+
         if(callback) {
           process.nextTick(callback);
         }
@@ -130,14 +136,13 @@ module.exports = {
 
         process.stdin.on('end', function() {
           var params = JSON.parse(stdIn);
-          
-          var to_write_files = 0;
-          var written_files = 0;
-          var looped_through = false;
+
+          var num_files = 0;
+          var num_written_files = 0;
 
           var checkForEnd = function(){
-            written_files++;
-            if (looped_through && (to_write_files == written_files)){
+            num_written_files++;
+            if (num_files == num_written_files){
               callback.call();
             }
           }
@@ -148,57 +153,64 @@ module.exports = {
 
           if (!(("_blockspring_spec" in params) && params._blockspring_spec)){
             request.params = params;
-          } else {
-            Object.keys(params).forEach(function(var_name){
-              if (var_name == "_blockspring_spec"){
-                // pass
-              } else if ((var_name == "_errors") && (toType( params[var_name] ) == "array")){
-                params[var_name].forEach(function(error){
-                  if((toType( error ) == "object") && ("title" in error)){
-                    request.addError(error);
-                  }
-                });
-              } else if (
-                  (toType(params[var_name]) == "object") &&
-                  ("filename" in params[var_name]) &&
-                  (params[var_name].filename) &&
-                  ((("data" in params[var_name]) && params[var_name].data) ||
-                  (("url" in params[var_name]) && params[var_name].url))
-              ) {
-                to_write_files++;
-                tmp.tmpName({postfix: '-' + params[var_name].filename }, function _tempNameGenerated(err, tmp_name) {
-                  if (err) {
-                    request.params[var_name] = params[var_name];
-                    checkForEnd();
-                  } else {
-                    if ("data" in params[var_name]){
-                      fs.writeFile(tmp_name, params[var_name].data, {encoding:'base64'}, function(err){
-                        if(err){
-                          request.params[var_name] = params[var_name]
-                        } else {
-                          request.params[var_name] = tmp_name;
-                        }
-                        checkForEnd();
-                      });
-                    } else {
-                      var file = fs.createWriteStream(tmp_name);
-                      requests(params[var_name].url).pipe(file);
-                      file.on('finish', function() {
-                        request.params[var_name] = tmp_name;
-                        checkForEnd();
-                      });
-                    }
-                  }
-                });
-              } else {
-                request.params[var_name] = params[var_name]
-              }
-            });
+            return;
           }
 
-          looped_through = true;
+          Object.keys(params).forEach(function(var_name){
 
-          if ((looped_through===true) && (to_write_files == written_files)){
+            // Ignore blockspring_spec flag
+            if (var_name == "_blockspring_spec"){
+              return;
+            }
+            // Add any errors to the request object
+            else if ((var_name == "_errors") && (toType( params[var_name] ) == "array")){
+              params[var_name].forEach(function(error){
+                if((toType( error ) == "object") && ("title" in error)){
+                  request.errors.push(error);
+                }
+              });
+            }
+            // Process file spec
+            else if (
+                toType(params[var_name]) == "object" &&
+                "filename" in params[var_name] &&
+                params[var_name].filename &&
+                (("data" in params[var_name]) && params[var_name].data) ||
+                  ("url" in params[var_name]) && params[var_name].url))
+            ) {
+              num_files++;
+              tmp.tmpName({postfix: '-' + params[var_name].filename }, function _tempNameGenerated(err, tmp_name) {
+                if (err) {
+                  request.params[var_name] = params[var_name];
+                  checkForEnd();
+                } else {
+                  if ("data" in params[var_name]){
+                    fs.writeFile(tmp_name, params[var_name].data, {encoding:'base64'}, function(err){
+                      if(err){
+                        request.params[var_name] = params[var_name]
+                      } else {
+                        request.params[var_name] = tmp_name;
+                      }
+                      checkForEnd();
+                    });
+                  } else {
+                    var file = fs.createWriteStream(tmp_name);
+                    requests(params[var_name].url).pipe(file);
+                    file.on('finish', function() {
+                      request.params[var_name] = tmp_name;
+                      checkForEnd();
+                    });
+                  }
+                }
+              });
+            }
+            // Otherwise treat it as a normal input
+            else {
+              request.params[var_name] = params[var_name];
+            }
+          });
+
+          if (num_files == num_written_files){
             callback.call();
           }
         });
